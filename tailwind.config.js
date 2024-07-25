e0cfc2a09578549af8f0e08d6eaf66d4341f66f8
“naukhaizAnjum” <naukhaiz.anjum@gmail.com>
2024-07-25 22:07:47 +0500

    Added hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
+// module.exports = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   darkMode: "class",
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
@@ -34,6 +34,21 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {},
+    animation: {
+      spotlight: "spotlight 2s ease .75s 1 forwards",
+    },
+    keyframes: {
+      spotlight: {
+        "0%": {
+          opacity: 0,
+          transform: "translate(-72%, -62%) scale(0.5)",
+        },
+        "100%": {
+          opacity: 1,
+          transform: "translate(-50%,-40%) scale(1)",
+        },
+      },
+    },
   },
   plugins: [addVariablesForColors],
 };
